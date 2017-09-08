6aa5b83a24413b974eb2b53c1f2f4a47ec3169b8
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-08 01:02:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-musicians = new Array();
-instrument = new Array();
 
 function theBeatlesPlay(musicians, instruments) {
   var array = new Array();
   for (var i = 0; i < musicians.length; i++) {
-    array.push(`${musicians} plays ${instruments}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
+}
+
+function johnLennonFacts(array) {
+  var array = new Array();
+  for (var i = 0; i < array.length; i++) {
+    array.push('${array[i]}')
+  }
+  return
 }
